2f48ff98bc964fb898742c87468275ca3b45885f
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2024-04-22 14:47:22 -0230

    Major change: Add code to make new figures

--- mean_stderrors_andplots_KL.R
+++ mean_stderrors_andplots_KL.R
@@ -51,7 +51,7 @@ Mean_Bio_BT
 
 library(ggplot2)
 
-jpeg("mean_BTbio_by_year.jpg", width=8, height=5, units='in', res=600)
+#jpeg("mean_BTbio_by_year.jpg", width=8, height=5, units='in', res=600)
 
 ggplot(Mean_Bio_BT, aes(as.factor(Year), mean, colour=BT_Gen)) + 
   geom_point(size=4, position=position_dodge(1)) +
@@ -99,7 +99,7 @@ d2<-subset(Mean_Den_BT, Year=="1989"&Type=="Compensation")
 
 
 
-jpeg("mean_BTden_by_year.jpg", width=8, height=5, units='in', res=600)
+#jpeg("mean_BTden_by_year.jpg", width=8, height=5, units='in', res=600)
 
 ggplot(Mean_Den_BT, aes(as.factor(Year), mean)) + 
   geom_point(size=3, position=position_dodge(1)) +
@@ -286,79 +286,198 @@ plot(BT1.glm)
 
 # new fig ----
 # by Riffles
-tmp <- edited_cs_estimates_by_site |>
-  filter(Pool == "No" & Species == "BT") |>
-  group_by(Year, Type) |>
-  summarise(N  = length(Biomass_100),
-                     mean = mean(Biomass_100),
-                     sd   = sd(Biomass_100),
-                     se   = sd / sqrt(N))
+# tmp <- edited_cs_estimates_by_site |>
+#   filter(Pool == "No" & Species == "BT") |>
+#   group_by(Year, Type) |>
+#   summarise(N  = length(Biomass_100),
+#                      mean = mean(Biomass_100),
+#                      sd   = sd(Biomass_100),
+#                      se   = sd / sqrt(N))
+# 
+#   tmp
+#   tmp$Type <- as.factor(tmp$Type)
+#   levels(tmp$Type) <- c("Compensation", "Control", "Compensation", "Downstream")
 
-  tmp
-  tmp$Type <- as.factor(tmp$Type)
-  levels(tmp$Type) <- c("Compensation", "Control", "Compensation", "Downstream")
+
   
-  library(ggplot2)
-p1 <- ggplot(tmp, aes(as.factor(Year), mean)) + 
-  geom_point(size=4, position=position_dodge(1)) +
-  theme_bw() + 
-  theme(axis.text.x  = element_text(angle=90, vjust=0.4, size=10)) +
-  facet_grid(~Type) +
-  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.5, position=position_dodge(1)) +
-  #ylab("Mean Biomass Estimate (g/100 sq. m)") +
-  ylab("") +
-  xlab("Year") +
-  theme(panel.grid.minor=element_blank(),
-        panel.grid.major=element_blank()) +
-  geom_vline(xintercept = 3, linetype = 3)
-
+# p1 <- ggplot(tmp, aes(as.factor(Year), mean)) + 
+#   geom_point(size=4, position=position_dodge(1)) +
+#   theme_bw() + 
+#   theme(axis.text.x  = element_text(angle=90, vjust=0.4, size=10)) +
+#   facet_grid(~Type) +
+#   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.5, position=position_dodge(1)) +
+#   #ylab("Mean Biomass Estimate (g/100 sq. m)") +
+#   ylab("") +
+#   xlab("Year") +
+#   theme(panel.grid.minor=element_blank(),
+#         panel.grid.major=element_blank()) +
+#   geom_vline(xintercept = 3, linetype = 3)
+# p1
 
 # by Pools
-tmp1 <- edited_cs_estimates_by_site |>
-  filter(Pool == "Yes" & Species == "BT") |>
-  group_by(Year, Type) |>
-  summarise(N  = length(Biomass_100),
-            mean = mean(Biomass_100),
-            sd   = sd(Biomass_100),
-            se   = sd / sqrt(N)
-            )
-head(edited_cs_estimates_by_site[, c(1:4, 6:7, 10:13)])
-tmp1
+# tmp1 <- edited_cs_estimates_by_site |>
+#   filter(Pool == "Yes" & Species == "BT") |>
+#   group_by(Year, Type) |>
+#   summarise(N  = length(Biomass_100),
+#             mean = mean(Biomass_100),
+#             sd   = sd(Biomass_100),
+#             se   = sd / sqrt(N)
+#             )
+# head(edited_cs_estimates_by_site[, c(1:4, 6:7, 10:13)])
+# tmp1
+# 
+# # tmp[tmp$Type == "Downstream",]
+# # tmp[tmp$Type == "Compensation",]
+# # edited_cs_estimates_by_site[edited_cs_estimates_by_site$Type == "Downstream" & edited_cs_estimates_by_site$Species == "BT",]
+# 
+# 
+# tmp1$Type <- as.factor(tmp1$Type)
+# levels(tmp1$Type) <- c("Compensation", "Compensation")
+# tmp_row <- tmp1[1,]
+# tmp_row[1,] <- NA
+# tmp_row$Year <- 1990
+# tmp_row$Type <- "Compensation"
+# tmp_row$Type <- as.factor(tmp_row$Type)
+# tmp_row$Year <- as.factor(tmp_row$Year)
+# tmp1 <- rbind(tmp1, tmp_row)
 
-tmp[tmp$Type == "Downstream",]
-tmp[tmp$Type == "Compensation",]
-edited_cs_estimates_by_site[edited_cs_estimates_by_site$Type == "Downstream" & edited_cs_estimates_by_site$Species == "BT",]
+# p2 <- ggplot(tmp1, aes(as.factor(Year), mean)) + 
+#   geom_point(size=4, position=position_dodge(1)) +
+#   theme_bw() + 
+#   theme(axis.text.x  = element_text(angle=90, vjust=0.4, size=10)) +
+#   facet_grid(~Type) +
+#   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.5, position=position_dodge(1)) +
+# #  ylab("Mean Biomass Estimate (g/100 sq. m)") +
+#   ylab("") +
+#   xlab("Year") +
+#   theme(panel.grid.minor=element_blank(),
+#         panel.grid.major=element_blank()) +
+#   geom_vline(xintercept = 3, linetype = 3)
 
-
-tmp1$Type <- as.factor(tmp1$Type)
-levels(tmp1$Type) <- c("Compensation", "Compensation")
-tmp_row <- tmp1[1,]
-tmp_row[1,] <- NA
-tmp_row$Year <- 1990
-tmp_row$Type <- "Compensation"
-tmp1 <- rbind(tmp1, tmp_row)
-
-p2 <- ggplot(tmp1, aes(as.factor(Year), mean)) + 
-  geom_point(size=4, position=position_dodge(1)) +
-  theme_bw() + 
-  theme(axis.text.x  = element_text(angle=90, vjust=0.4, size=10)) +
-  facet_grid(~Type) +
-  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.5, position=position_dodge(1)) +
-#  ylab("Mean Biomass Estimate (g/100 sq. m)") +
-  ylab("") +
-  xlab("Year") +
-  theme(panel.grid.minor=element_blank(),
-        panel.grid.major=element_blank()) +
-  geom_vline(xintercept = 3, linetype = 3)
+source("glmm_fun.R")
+library(dplyr)
+library(ggplot2)
 library(cowplot)
+
+# biomass ----
+# BT
+bt.bio.np.summ <- fig.data(edited_cs_estimates_by_site, "No", "BT", Biomass_100)
+bt.bio.pl.summ <- fig.data(edited_cs_estimates_by_site, "Yes", "BT", Biomass_100)
+p1 <- fig.np(bt.bio.np.summ)
+p2 <- fig.np(bt.bio.pl.summ)
 
 bottom_row <- plot_grid(p2, NULL, NULL, rel_widths = c(1.15, 1, 1),ncol = 3)
 #plot_grid(p1, bottom_row, nrow = 2)
-
-
 
 plot_grid(plot_grid(p1, bottom_row, labels=c("", ""), nrow = 2), scale=0.9) +
 #draw_label("Mean biomass estimate (g/100 sq. m)", x=  0, y=0.5, vjust= 1.5, angle=90) +
 draw_label("Biomass (g/100 sq. m)", x=  0.03, y=0.5, vjust= 1.5, angle=90)
 ggsave(paste0("output/BT_biomass.png"), width=10, height=8, units="in")
 
+
+# BTYOY
+btyoy.bio.np.summ <- fig.data(edited_cs_estimates_by_site, "No", "BTYOY", Biomass_100)
+btyoy.bio.pl.summ <- fig.data(edited_cs_estimates_by_site, "Yes", "BTYOY", Biomass_100)
+p3 <- fig.np(btyoy.bio.np.summ)
+p4 <- fig.np(btyoy.bio.pl.summ)
+
+bottom_row <- plot_grid(p4, NULL, NULL, rel_widths = c(1.15, 1, 1),ncol = 3)
+#plot_grid(p1, bottom_row, nrow = 2)
+
+plot_grid(plot_grid(p6, bottom_row, labels=c("", ""), nrow = 2), scale=0.9) +
+  #draw_label("Mean biomass estimate (g/100 sq. m)", x=  0, y=0.5, vjust= 1.5, angle=90) +
+  draw_label("Biomass (g/100 sq. m)", x=  0.03, y=0.5, vjust= 1.5, angle=90)
+ggsave(paste0("output/BTYOY_biomass.png"), width=10, height=8, units="in")
+
+
+
+# AS
+as.bio.np.summ <- fig.data(edited_cs_estimates_by_site, "No", "AS ", Biomass_100)
+as.bio.pl.summ <- fig.data(edited_cs_estimates_by_site, "Yes", "AS ", Biomass_100)
+p5 <- fig.np(as.bio.np.summ)
+p6 <- fig.np(as.bio.pl.summ)
+
+bottom_row <- plot_grid(p6, NULL, NULL, rel_widths = c(1.15, 1, 1),ncol = 3)
+#plot_grid(p1, bottom_row, nrow = 2)
+
+plot_grid(plot_grid(p5, bottom_row, labels=c("", ""), nrow = 2), scale=0.9) +
+  #draw_label("Mean biomass estimate (g/100 sq. m)", x=  0, y=0.5, vjust= 1.5, angle=90) +
+  draw_label("Biomass (g/100 sq. m)", x=  0.03, y=0.5, vjust= 1.5, angle=90)
+ggsave(paste0("output/AS_biomass.png"), width=10, height=8, units="in")
+
+
+# ASYOY
+asyoy.bio.np.summ <- fig.data(edited_cs_estimates_by_site, "No", "AS ", Biomass_100)
+asyoy.bio.pl.summ <- fig.data(edited_cs_estimates_by_site, "Yes", "AS ", Biomass_100)
+p7 <- fig.np(asyoy.bio.np.summ)
+p8 <- fig.np(asyoy.bio.pl.summ)
+
+bottom_row <- plot_grid(p8, NULL, NULL, rel_widths = c(1.15, 1, 1),ncol = 3)
+#plot_grid(p1, bottom_row, nrow = 2)
+
+plot_grid(plot_grid(p7, bottom_row, labels=c("", ""), nrow = 2), scale=0.9) +
+  #draw_label("Mean biomass estimate (g/100 sq. m)", x=  0, y=0.5, vjust= 1.5, angle=90) +
+  draw_label("Biomass (g/100 sq. m)", x=  0.03, y=0.5, vjust= 1.5, angle=90)
+ggsave(paste0("output/ASYOY_biomass.png"), width=10, height=8, units="in")
+
+# density ----
+
+# BT
+bt.den.np.summ <- fig.data(edited_cs_estimates_by_site, "No", "BT", Density_100)
+bt.den.pl.summ <- fig.data(edited_cs_estimates_by_site, "Yes", "BT", Density_100)
+p9 <- fig.np(bt.den.np.summ)
+p10 <- fig.np(bt.den.pl.summ)
+
+bottom_row <- plot_grid(p10, NULL, NULL, rel_widths = c(1.15, 1, 1),ncol = 3)
+#plot_grid(p1, bottom_row, nrow = 2)
+
+plot_grid(plot_grid(p9, bottom_row, labels=c("", ""), nrow = 2), scale=0.9) +
+  #draw_label("Mean biomass estimate (g/100 sq. m)", x=  0, y=0.5, vjust= 1.5, angle=90) +
+  draw_label("Density (# fish/100 sq. m)", x=  0.03, y=0.5, vjust= 1.5, angle=90)
+ggsave(paste0("output/BT_density.png"), width=10, height=8, units="in")
+
+
+# BTYOY
+btyoy.den.np.summ <- fig.data(edited_cs_estimates_by_site, "No", "BTYOY", Density_100)
+btyoy.den.pl.summ <- fig.data(edited_cs_estimates_by_site, "Yes", "BTYOY", Density_100)
+p11 <- fig.np(btyoy.den.np.summ)
+p12 <- fig.np(btyoy.den.pl.summ)
+
+bottom_row <- plot_grid(p12, NULL, NULL, rel_widths = c(1.15, 1, 1),ncol = 3)
+#plot_grid(p1, bottom_row, nrow = 2)
+
+plot_grid(plot_grid(p11, bottom_row, labels=c("", ""), nrow = 2), scale=0.9) +
+  #draw_label("Mean biomass estimate (g/100 sq. m)", x=  0, y=0.5, vjust= 1.5, angle=90) +
+  draw_label("Density (# fish/100 sq. m)", x=  0.03, y=0.5, vjust= 1.5, angle=90)
+ggsave(paste0("output/BTYOY_density.png"), width=10, height=8, units="in")
+
+
+
+# AS
+as.den.np.summ <- fig.data(edited_cs_estimates_by_site, "No", "AS ", Density_100)
+as.den.pl.summ <- fig.data(edited_cs_estimates_by_site, "Yes", "AS ", Density_100)
+p13 <- fig.np(as.den.np.summ)
+p14 <- fig.np(as.den.pl.summ)
+
+bottom_row <- plot_grid(p14, NULL, NULL, rel_widths = c(1.15, 1, 1),ncol = 3)
+#plot_grid(p1, bottom_row, nrow = 2)
+
+plot_grid(plot_grid(p13, bottom_row, labels=c("", ""), nrow = 2), scale=0.9) +
+  #draw_label("Mean biomass estimate (g/100 sq. m)", x=  0, y=0.5, vjust= 1.5, angle=90) +
+  draw_label("Density (# fish/100 sq. m)", x=  0.03, y=0.5, vjust= 1.5, angle=90)
+ggsave(paste0("output/AS_density.png"), width=10, height=8, units="in")
+
+
+# ASYOY
+asyoy.den.np.summ <- fig.data(edited_cs_estimates_by_site, "No", "AS ", Density_100)
+asyoy.den.pl.summ <- fig.data(edited_cs_estimates_by_site, "Yes", "AS ", Density_100)
+p15 <- fig.np(asyoy.den.np.summ)
+p16 <- fig.np(asyoy.den.pl.summ)
+
+bottom_row <- plot_grid(p16, NULL, NULL, rel_widths = c(1.15, 1, 1),ncol = 3)
+#plot_grid(p1, bottom_row, nrow = 2)
+
+plot_grid(plot_grid(p15, bottom_row, labels=c("", ""), nrow = 2), scale=0.9) +
+  #draw_label("Mean biomass estimate (g/100 sq. m)", x=  0, y=0.5, vjust= 1.5, angle=90) +
+  draw_label("Density (# fish/100 sq. m)", x=  0.03, y=0.5, vjust= 1.5, angle=90)
+ggsave(paste0("output/ASYOY_density.png"), width=10, height=8, units="in")
